533c82b4079dbe58f7478dcb084b1e000a1c675b
hindla1 <indlaharibabu@gmail.com>
2022-06-21 07:35:36 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 variable "ami"{}
 variable "instance_type"{}
-
+variable "ACCESS_KEY" {}
+variable "SECRET_KEY" {}
 resource "aws_instance" "my-ec2"{
    ami = var.ami
    instance_type = var.instance_type
